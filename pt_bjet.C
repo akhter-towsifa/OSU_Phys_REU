487f603b66d19b7068c345db957eeb1dbe623865
toraneus <51368122+toraneus@users.noreply.github.com>
2019-06-11 15:07:59 -0500

    Add files via upload

--- pt_bjet.C
+++ pt_bjet.C
@@ -3,6 +3,9 @@
 #include <vector>
 #include "TH1F.h"
 #include "TH1I.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "iostream"
 
 
 /*This code stores all the jets that have a b particle in it. Then it selects
@@ -18,7 +21,7 @@ void pt_bjet(){
   auto h_jet_pt_btag = new TH1I("jet_btagged","Jet p_{T} b-tagged;X",nbins,0.0, 2.4e6);
   auto h_jet_pt = new TH1F("jet_pt","Jet_P_{T}; X; Counts",nbins,0.0,2.4e6);
   auto file = TFile::Open("bbww_x1000_s170.root");
-  
+
   TTree* ctree = (TTree*)file->Get("CollectionTree");
   
   Int_t           n_jet;
@@ -45,32 +48,47 @@ void pt_bjet(){
   }
 
   int64_t bj[14] = {};		//stores the index of events with b-jet(s).
-  int64_t pt_bjet_all = {};		//stores the pt of the events with greater than 2 b-jets.
-  int64_t pt_bjet_2jets = {};		//stores the pt of the events with 2 b-jets with the greatest energy.
+  int pt_bjet_all[14] = {};	//stores the pt of the events with greater than 2 b-jets.
+  int64_t pt_bjet_2jets[14] = {};//stores the pt of the events with 2 b-jets with the greatest energy.
+
 // This part creates the jet_pt histogram of all the b-jets.  
   for (int i=0; i<nentries;i++){
     ctree->GetEntry(i);
     int nbtags = 0; 
+    
     for (int64_t j=0; j<n_jet; j++) {
       if(jet_btagged[j]>0){         
 	bj[nbtags]=j;
         nbtags++;
-	h_jet_pt_btag->Fill(jet_pt[j], w);
-	cout << "\t pt_bjet \t" << jet_pt[j] << "\tj\t" << j << endl;
-	
-	if (j >=2) {
-	  pt_bjet = jet_pt[j];
-	  if (j > 2) {
-	    for (int k=0; k<j+1; j++){
-	      if (pt_bjet[k]
-	    }
-	  }
-	  else {
-	    pt_bjet_2jets = pt_bjet
-	}
+	h_jet_pt_btag->Fill(jet_pt[j], w);	
       }
+      pt_bjet_all[i] = jet_pt[j];
+
+	
+      int n = sizeof(pt_bjet_all)/sizeof(pt_bjet_all[0]);
+      //cout << "pt_bjet_all\t\t" << pt_bjet_all[i] << endl;
+      sort(pt_bjet_all, pt_bjet_all+n, greater<int>());
     }
+    //cout << "sorted pt_bjet_all\t\t" << i << "\t\t" << pt_bjet_all[i] << endl;
+    
+    int size_pt_bjet_all = sizeof(pt_bjet_all)/sizeof(pt_bjet_all[0]);
+    //cout << "\t\tsize of pt_bjet\t\t" << size_pt_bjet_all << endl;
+    
+    
+    
+    if (size_pt_bjet_all == 2){
+      pt_bjet_2jets[i] = pt_bjet_all[i];
+      cout << "pt bjet 2 jets\t\t" << pt_bjet_2jets[i] << endl;
+    }
+    else if (size_pt_bjet_all > 2){
+      for (int k=0; k< 2 ; k++){
+	pt_bjet_2jets[i] = pt_bjet_all[k];
+      }
+      //cout << "pt bjet 2 jets\t\t\t" << pt_bjet_2jets[i] << endl;
+    }
+    
   }
+
   
   
   
