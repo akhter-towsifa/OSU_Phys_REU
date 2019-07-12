332340afac7e53cc5722cc5a102fddbc1f3157f0
toraneus <51368122+toraneus@users.noreply.github.com>
2019-07-12 15:05:23 -0500

    Add files via upload

--- a_1.C
+++ a_1.C
@@ -24,7 +24,12 @@ void a_1(){
   int w = 1;				//weight
   
   auto h_Minv = new TH1F("M_{inv}", "Invariant Mass of the Higgs Boson and W from large jet; Mass [MeV]", nbins, 0.0, 2.0e5);
-    
+  auto h_Higgs = new TH1F("M_{inv} Higgs", "Invariant Mass of the Higgs Boson from large jet; Mass [MeV]", nbins, 0.0, 2.0e5);
+  auto h_W1 = new TH1F("M_{inv} W1", "Invariant Mass of first W Boson from large jet; Mass [MeV]", nbins, 0.0, 2.0e5);
+  auto h_W2 = new TH1F("M_{inv} W2", "Invariant Mass of second W Boson from large jet; Mass [MeV]", nbins, 0.0, 2.0e5);
+  auto h_S = new TH1F("M_{inv} S", "Invariant Mass of the S particle from W's; Mass [MeV]", nbins, 0.0, 3.0e6);
+  auto h_X = new TH1F("M_{inv} X", "Invariant Mass of the X Particle from large jet Higgs and W; Mass [MeV]", nbins, 0.0, 3.0e6);
+  
   auto file = TFile::Open("bbww_x2000_s1500.root");
   TTree* ctree = (TTree*)file->Get("CollectionTree");
   
@@ -72,6 +77,15 @@ void a_1(){
   for (int i=0; i<nentries; i++)			//loops over all the 20,000 entries.
   {
     ctree->GetEntry(i);
+    TLorentzVector H;					//Higgs Lorentz Vector
+    TLorentzVector W1;					//First W from ljet2
+    TLorentzVector W2;					//Second W from ljet3
+    TLorentzVector S;					//S Lorentz Vector
+    TLorentzVector X;					//X Lorentz Vector
+
+    
+    int count_w = 0;					//counting the number of W's
+    float ljet_compare = 0;				//to seclude the higgs boson from the W's
     
     for (int j=0; j<n_ljet; j++)			//loops over all the large jets in each entry
     {
@@ -79,17 +93,68 @@ void a_1(){
       ljet.SetPtEtaPhiE(ljet_pt[j], ljet_eta[j], ljet_phi[j], ljet_e[j]);
                   
       if (ljet_pt[j] >= 250e3 && abs(ljet_eta[j]) <= 2.0)
-      {
-	  h_Minv->Fill(ljet.M(), w);
+      {	
+	h_Minv->Fill(ljet.M(), w);
+	
+	if (ljet.M() > 105e3 && ljet.M() < 145e3)
+	{
+	  H.SetPtEtaPhiE(ljet_pt[j], ljet_eta[j], ljet_phi[j], ljet_e[j]);
+	  h_Higgs->Fill(H.M(), w);
+	}
+	
+	if (ljet.M() > 60e3 && ljet.M() < 100e3)
+	{
+	  count_w++;
+	  if (count_w == 1)
+	  {
+	    W1.SetPtEtaPhiE(ljet_pt[j], ljet_eta[j], ljet_phi[j], ljet_e[j]);
+	    h_W1->Fill(W1.M(), w);
+	  }
+	  else
+	  {
+	    W2.SetPtEtaPhiE(ljet_pt[j], ljet_eta[j], ljet_phi[j], ljet_e[j]);
+	    h_W2->Fill(W2.M(), w);
+	  }
+	}
+	S = W1 + W2;
+	h_S->Fill(S.M(), w);
+	
       }
     }  
+    X = H + S;
+    h_X->Fill(X.M(), w);  
   } 
   
   file->Close();
   TFile f("a_1.root", "recreate");
   
   h_Minv -> Write();
+  h_Higgs -> Write();
+  h_W1 -> Write();
+  h_W2 -> Write();
+  h_S -> Write();
+  h_X -> Write();
+  
+  c = new TCanvas("canvas", "M_Higgs_S_X", 2000, 1000);
+  c -> Divide (3,2);
+  
+  c -> cd(1);
   h_Minv -> Draw();
+  
+  c -> cd(2);
+  h_Higgs -> Draw();
+  
+  c -> cd(3);
+  h_W1 -> Draw();
+  
+  c -> cd(4);
+  h_W2 -> Draw();
+  
+  c -> cd(5);
+  h_S -> Draw();
+  
+  c -> cd(6);
+  h_X -> Draw();
   
   f.Close();
 } 
